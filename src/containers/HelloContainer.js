f68d9df748787fc3628e49dbd548e7e3ec190a74
Hugueds <hugo-pdf@hotmail.com>
2018-07-16 21:59:03 -0300

    Add Hello component

---
+++ src/containers/HelloContainer.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { Hello } from "./../components/Hello";
+
+export class HelloContainer extends React.Component {
+    render() {
+        return <Hello name="Hugo" />
+    }
+}
--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { HelloContainer } from "../src/containers/HelloContainer";
 
 const Index = () => {
-  return <div>Hello React!</div>;
+  return <HelloContainer />;
 };
 
 ReactDOM.render(<Index />, document.getElementById("app"));
